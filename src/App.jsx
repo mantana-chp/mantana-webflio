5bd28a29c2f6a2e839b8dcc3bbbff652d79a1df1
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-10-06 21:17:47 +0700

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+function NavBar() {
+  return (
+    <div>
+      <h1 className="text-red-800">Nav bar</h1>
+    </div>
+  )
+}
+export default NavBar
